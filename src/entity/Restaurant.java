a8bdef66a80fc15848534e87fabe3e049179f67d
LRistiana <liangga4@gmail.com>
2023-04-13 00:03:14 +0800

    menambahkan opsi menambahkan menu di constructor restaurant

--- src/entity/Restaurant.java
+++ src/entity/Restaurant.java
@@ -1,7 +1,7 @@
 package entity;
 
 import java.util.*;
-
+import static utility.Input.*;
 public class Restaurant {
     private static int totalRestaurant = 0;
     private int idRestaurant;
@@ -15,6 +15,12 @@ public class Restaurant {
         this.alamat = alamat;
         totalRestaurant++;
         this.idRestaurant = totalRestaurant;
+        while(inputInt("Apakah ingin menambahkan menu?\n[1]Ya [0] Tidak",0,1) == 1){
+//            if (inputInt("Apakah ingin menambahkan menu?\n[1]Ya [0] Tidak",0,1) == 0){
+//                break;
+//            }
+            this.addMenu(inputString("Masukan nama menu"),inputInt("Masukan harganya",0));
+        }
     }
     public int getIdRestaurant(){
         return this.idRestaurant;
@@ -40,19 +46,22 @@ public class Restaurant {
         this.menu.add(menu);
         this.harga.add(harga);
 
+
     }
     public void showMenu(){
         if (menu.size() == 0){
-            System.out.println("Restauran ini tidak memiliki menu");
+            System.out.println("\tRestauran ini tidak memiliki menu");
         }else {
+            System.out.println("\tMenu :");
             for (int i = 0; i < menu.size(); i++) {
-                System.out.printf("%s\t\t%d", menu.get(i), harga.get(i));
+                System.out.printf("\t\t- %s\t\tRp.%d\n", menu.get(i), harga.get(i));
             }
         }
     }
     
     public void showData(){
         System.out.println(this.nama);
+        System.out.println("\talamat : " + this.alamat);
         showMenu();
         System.out.println();
     }
