df68202f16aa5061b2517966b28e2cb09284f68a
LRistiana <liangga4@gmail.com>
2023-04-12 23:01:16 +0800

    menerapkan utility input

--- src/menu/MenuAdmin.java
+++ src/menu/MenuAdmin.java
@@ -2,64 +2,67 @@ package menu;
 
 import entity.Restaurant;
 
-import java.sql.SQLOutput;
+import static utility.Input.*;
 import java.util.*;
 
 class MenuAdmin {
-    Scanner scan = new Scanner(System.in);
     private static ArrayList<Restaurant>Restaurants = new ArrayList<Restaurant>();
-    public static void mainAdmin(){
-        System.out.println("Login Succes!");
-        System.exit(0);
 
+    public static void main(String[] args){
+
+
+        System.out.println("Login Succes!");
         System.out.println("Menu Admin");
         System.out.println("1. Lihat Restaurant");
         System.out.println("2. Tambah Restaurant");
         System.out.println("3. Hapus Restaurant");
-        int pilihan = this.scan.nextInt();
+        int pilihan  = inputInt("Masukan Pilihan",1,3);
         switch (pilihan){
             case 1 :
-                lihatRestauran();
+                lihatRestaurant();
+                break;
             case 2 :
                 tambahRestaurant();
+                break;
             case 3 :
                 hapusRestaurant();
+                break;
             default:
                 System.out.println("menu tidak tersedia");
         }
 
     }
-    private static void lihatRestauran(){
-        for (int i = 0; i < Restaurants.size(); i++) {
-            Restaurants.get(i).showData();
+    private static void lihatRestaurant(){
+        if (Restaurants.size() != 0){
+            for (int i = 0; i < Restaurants.size(); i++) {
+                System.out.printf("%d.\t",i+1);
+                Restaurants.get(i).showData();
+            }
+        }else {
+            System.out.println("Tidak ada restaurant");
         }
 
     }
     private static void tambahRestaurant(){
-        String nama, alamat;
 
+        String nama = inputString("Masukan Nama Restaurant baru");
+        String alamat = inputString("Masukan alamat restaurant " + nama);
 
-        System.out.println("MASUKAN NAMA RESTAURANT");
-        nama = this.scan.nextLine();
-        System.out.println("MASUKAN ALAMAT RESTAURANT");
-        alamat = this.scan.nextLine();
         Restaurants.add(new Restaurant(nama,alamat));
-
+        lihatRestaurant();
+        System.exit(0);
     }
     private static void hapusRestaurant(){
         System.out.println("Nama Restaurant");
-        lihatRestauran();
+        lihatRestaurant();
 
-        System.out.println("Masukan Nomer Restaurant yang ingin di hapus");
-        int resDelete = this.scan.nextInt();
 
-        System.out.println("apakah anda yakin?\n[1]Ya\n[2]Tidak");
-        int confirm = this.scan.nextInt();
+        int resDelete = inputInt("Masukan Nomer Restauran yang ingin dihapus",1,Restaurants.size());
+        int confirm = inputInt("apakah anda yakin?\n[1]Ya\n[2]Tidak",1,2);
         if (confirm == 1){
             hapusRestaurant();
         }
         Restaurants.remove(resDelete-1);
-
     }
 
 }
